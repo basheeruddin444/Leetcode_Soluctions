2dbb59bb9dead4ada46d066ea5e6e98bcbf33f79
basheer <basheer13092005@gmail.com>
2026-08-08 07:46:15 +0530

    Time: 554 ms (80.2%), Space: 0B (100%) - LeetHub

---
+++ 0262-trips-and-users/0262-trips-and-users.sql
@@ -0,0 +1,17 @@
+# Write your MySQL query statement below
+SELECT
+    t.request_at AS Day,
+    ROUND(
+        SUM(t.status != 'completed') / COUNT(*),
+        2
+    ) AS `Cancellation Rate`
+FROM Trips t
+JOIN Users u1
+    ON t.client_id = u1.users_id
+JOIN Users u2
+    ON t.driver_id = u2.users_id
+WHERE u1.banned = 'No'
+  AND u2.banned = 'No'
+  AND t.request_at BETWEEN '2013-10-01' AND '2013-10-03'
+GROUP BY t.request_at
+ORDER BY t.request_at;
